11d2a351b4653c4d74e30edb526c775c3f9733b2
wuchengxing <1449697757@qq.com>
2022-07-29 21:35:03 +0800

    feat：策略数据相关的初始化sql

---
+++ simple-lottery-services/src/test/java/com/simple/lottery/domain/StrategyTest.java
@@ -0,0 +1,19 @@
+package com.simple.lottery.domain;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 项目: simple-lottery
+ * <p>
+ * 功能描述: 策略相关测试
+ *
+ * @author: WuChengXing
+ * @create: 2022-07-29 17:23
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StrategyTest {
+
+}
