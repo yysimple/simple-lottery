bafb7b478ac0565fb9a0a9f9991356df528bb15e
wuchengxing <1449697757@qq.com>
2022-07-29 14:29:36 +0800

    feat：修改sql

--- docs/sql/ddl.sql
+++ docs/sql/ddl.sql
@@ -21,7 +21,7 @@ CREATE TABLE if not exists `activity`
     PRIMARY KEY (`id`),
     UNIQUE KEY `unique_activity_id` (`activity_id`)
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 2
+  AUTO_INCREMENT = 1
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_bin COMMENT ='活动配置';
 
@@ -30,13 +30,13 @@ create table if not exists award
 (
     id           bigint(11) auto_increment comment '自增ID'
         primary key,
-    awardId      bigint null comment '奖品ID',
-    awardType    int(4) null comment '奖品类型（文字描述、兑换码、优惠券、实物奖品暂无）',
-    awardCount   int null comment '奖品数量',
-    awardName    varchar(64) null comment '奖品名称',
-    awardContent varchar(128) null comment '奖品内容「文字描述、Key、码」',
-    createTime   datetime default CURRENT_TIMESTAMP null comment '创建时间',
-    updateTime   datetime default CURRENT_TIMESTAMP null comment '更新时间'
+    award_id      bigint null comment '奖品ID',
+    award_type    int(4) null comment '奖品类型（文字描述、兑换码、优惠券、实物奖品暂无）',
+    award_count   int null comment '奖品数量',
+    award_name    varchar(64) null comment '奖品名称',
+    award_content varchar(128) null comment '奖品内容「文字描述、Key、码」',
+    create_time   datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time   datetime default CURRENT_TIMESTAMP null comment '更新时间'
 ) comment '奖品配置';
 
 -- 策略配置
@@ -44,16 +44,16 @@ create table if not exists strategy
 (
     id           bigint(11) auto_increment comment '自增ID'
         primary key,
-    strategyId   bigint(11) not null comment '策略ID',
-    strategyDesc varchar(128) null comment '策略描述',
-    strategyMode int(4) null comment '策略方式「1:单项概率、2:总体概率」',
-    grantType    int(4) null comment '发放奖品方式「1:即时、2:定时[含活动结束]、3:人工」',
-    grantDate    datetime null comment '发放奖品时间',
-    extInfo      varchar(128) null comment '扩展信息',
-    createTime   datetime null comment '创建时间',
-    updateTime   datetime null comment '修改时间',
-    constraint strategy_strategyId_uindex
-        unique (strategyId)
+    strategy_id   bigint(11) not null comment '策略ID',
+    strategy_desc varchar(128) null comment '策略描述',
+    strategy_mode int(4) null comment '策略方式「1:单项概率、2:总体概率」',
+    grant_type    int(4) null comment '发放奖品方式「1:即时、2:定时[含活动结束]、3:人工」',
+    grant_date    datetime null comment '发放奖品时间',
+    ext_info      varchar(128) null comment '扩展信息',
+    create_time   datetime null comment '创建时间',
+    update_time   datetime null comment '修改时间',
+    constraint idx_strategy_id
+        unique (strategy_id)
 ) comment '策略配置';
 
 -- 策略明细
@@ -61,237 +61,237 @@ create table if not exists strategy_detail
 (
     id         bigint(11) auto_increment comment '自增ID'
         primary key,
-    strategyId bigint(11) not null comment '策略ID',
-    awardId    bigint(11) null comment '奖品ID',
-    awardCount int null comment '奖品数量',
-    awardRate  decimal(5, 2) null comment '中奖概率',
-    awardSurplusCount int default 0 null comment '奖品剩余库存',
-    createTime datetime null comment '创建时间',
-    updateTime datetime null comment '修改时间'
+    strategy_id bigint(11) not null comment '策略ID',
+    award_id    bigint(11) null comment '奖品ID',
+    award_count int null comment '奖品数量',
+    award_rate  decimal(5, 2) null comment '中奖概率',
+    award_surplus_count int default 0 null comment '奖品剩余库存',
+    create_time datetime null comment '创建时间',
+    update_time datetime null comment '修改时间'
 ) comment '策略明细';
 
 -- 另外两个库 --
-create schema if not exists simple_lottery_001;
-create schema if not exists simple_lottery_002;
+create schema if not exists simple_lottery_01;
+create schema if not exists simple_lottery_02;
 
 use
-simple_lottery_001;
+simple_lottery_01;
 
 -- 用户参与活动记录表
 create table if not exists user_take_activity
 (
     id           bigint(11) auto_increment comment '自增ID' primary key,
-    uId          tinytext null,
-    takeId       bigint null,
-    activityId   bigint null,
-    activityName tinytext null,
-    takeDate     timestamp null,
-    takeCount    int null,
+    u_id          tinytext null,
+    take_id       bigint null,
+    activity_id   bigint null,
+    activity_name tinytext null,
+    take_date     timestamp null,
+    take_count    int null,
     uuid         tinytext null,
-    createTime   timestamp null,
-    updateTime   timestamp null
+    create_time   timestamp null,
+    update_time   timestamp null
 ) comment '用户参与活动记录表';
 
 -- 用户活动参与次数表
 create table if not exists user_take_activity_count
 (
     id         bigint(11) auto_increment comment '自增ID' primary key,
-    uId        tinytext null,
-    activityId bigint null,
-    totalCount int null,
-    leftCount  int null,
-    createTime timestamp null,
-    updateTime timestamp null
+    u_id        tinytext null,
+    activity_id bigint null,
+    total_count int null,
+    left_count  int null,
+    create_time timestamp null,
+    update_time timestamp null
 ) comment '用户活动参与次数表';
 
 create table if not exists user_strategy_export_001
 (
     id           bigint(11) auto_increment comment '自增ID' primary key,
-    uId          mediumtext null,
-    activityId   bigint null,
-    orderId      bigint null,
-    strategyId   bigint null,
-    strategyType int null,
-    grantType    int null,
-    grantDate    timestamp null,
-    grantState   int null,
-    awardId      bigint null,
-    awardType    int null,
-    awardName    mediumtext null,
-    awardContent mediumtext null,
+    u_id          mediumtext null,
+    activity_id   bigint null,
+    order_id      bigint null,
+    strategy_id   bigint null,
+    strategy_type int null,
+    grant_type    int null,
+    grant_date    timestamp null,
+    grant_state   int null,
+    award_id      bigint null,
+    award_type    int null,
+    award_name    mediumtext null,
+    award_content mediumtext null,
     uuid         mediumtext null,
-    createTime   timestamp null,
-    updateTime   timestamp null
+    create_time   timestamp null,
+    update_time   timestamp null
 ) comment '用户策略计算结果表';
 
 create table if not exists user_strategy_export_002
 (
     id           bigint(11) auto_increment comment '自增ID' primary key,
-    uId          mediumtext null,
-    activityId   bigint null,
-    orderId      bigint null,
-    strategyId   bigint null,
-    strategyType int null,
-    grantType    int null,
-    grantDate    timestamp null,
-    grantState   int null,
-    awardId      bigint null,
-    awardType    int null,
-    awardName    mediumtext null,
-    awardContent mediumtext null,
+    u_id          mediumtext null,
+    activity_id   bigint null,
+    order_id      bigint null,
+    strategy_id   bigint null,
+    strategy_type int null,
+    grant_type    int null,
+    grant_date    timestamp null,
+    grant_state   int null,
+    award_id      bigint null,
+    award_type    int null,
+    award_name    mediumtext null,
+    award_content mediumtext null,
     uuid         mediumtext null,
-    createTime   timestamp null,
-    updateTime   timestamp null
+    create_time   timestamp null,
+    update_time   timestamp null
 ) comment '用户策略计算结果表';
 
 create table if not exists user_strategy_export_003
 (
     id           bigint(11) auto_increment comment '自增ID' primary key,
-    uId          mediumtext null,
-    activityId   bigint null,
-    orderId      bigint null,
-    strategyId   bigint null,
-    strategyType int null,
-    grantType    int null,
-    grantDate    timestamp null,
-    grantState   int null,
-    awardId      bigint null,
-    awardType    int null,
-    awardName    mediumtext null,
-    awardContent mediumtext null,
+    u_id          mediumtext null,
+    activity_id   bigint null,
+    order_id      bigint null,
+    strategy_id   bigint null,
+    strategy_type int null,
+    grant_type    int null,
+    grant_date    timestamp null,
+    grant_state   int null,
+    award_id      bigint null,
+    award_type    int null,
+    award_name    mediumtext null,
+    award_content mediumtext null,
     uuid         mediumtext null,
-    createTime   timestamp null,
-    updateTime   timestamp null
+    create_time   timestamp null,
+    update_time   timestamp null
 ) comment '用户策略计算结果表';
 
 create table if not exists user_strategy_export_004
 (
     id           bigint(11) auto_increment comment '自增ID' primary key,
-    uId          mediumtext null,
-    activityId   bigint null,
-    orderId      bigint null,
-    strategyId   bigint null,
-    strategyType int null,
-    grantType    int null,
-    grantDate    timestamp null,
-    grantState   int null,
-    awardId      bigint null,
-    awardType    int null,
-    awardName    mediumtext null,
-    awardContent mediumtext null,
+    u_id          mediumtext null,
+    activity_id   bigint null,
+    order_id      bigint null,
+    strategy_id   bigint null,
+    strategy_type int null,
+    grant_type    int null,
+    grant_date    timestamp null,
+    grant_state   int null,
+    award_id      bigint null,
+    award_type    int null,
+    award_name    mediumtext null,
+    award_content mediumtext null,
     uuid         mediumtext null,
-    createTime   timestamp null,
-    updateTime   timestamp null
+    create_time   timestamp null,
+    update_time   timestamp null
 ) comment '用户策略计算结果表';
 
 -- 第二个库
 use
-simple_lottery_002;
+simple_lottery_02;
 
 -- 用户参与活动记录表
 create table if not exists user_take_activity
 (
     id           bigint(11) auto_increment comment '自增ID' primary key,
-    uId          tinytext null,
-    takeId       bigint null,
-    activityId   bigint null,
-    activityName tinytext null,
-    takeDate     timestamp null,
-    takeCount    int null,
+    u_id          tinytext null,
+    take_id       bigint null,
+    activity_id   bigint null,
+    activity_name tinytext null,
+    take_date     timestamp null,
+    take_count    int null,
     uuid         tinytext null,
-    createTime   timestamp null,
-    updateTime   timestamp null
+    create_time   timestamp null,
+    update_time   timestamp null
 ) comment '用户参与活动记录表';
 
 -- 用户活动参与次数表
 create table if not exists user_take_activity_count
 (
     id         bigint(11) auto_increment comment '自增ID' primary key,
-    uId        tinytext null,
-    activityId bigint null,
-    totalCount int null,
-    leftCount  int null,
-    createTime timestamp null,
-    updateTime timestamp null
+    u_id        tinytext null,
+    activity_id bigint null,
+    total_count int null,
+    left_count  int null,
+    create_time timestamp null,
+    update_time timestamp null
 ) comment '用户活动参与次数表';
 
 create table if not exists user_strategy_export_001(
-    id           bigint(11) auto_increment comment '自增ID' primary key,
-    uId          mediumtext null,
-    activityId   bigint null,
-    orderId      bigint null,
-    strategyId   bigint null,
-    strategyType int null,
-    grantType    int null,
-    grantDate    timestamp null,
-    grantState   int null,
-    awardId      bigint null,
-    awardType    int null,
-    awardName    mediumtext null,
-    awardContent mediumtext null,
-    uuid         mediumtext null,
-    createTime   timestamp null,
-    updateTime   timestamp null
+   id           bigint(11) auto_increment comment '自增ID' primary key,
+   u_id          mediumtext null,
+   activity_id   bigint null,
+   order_id      bigint null,
+   strategy_id   bigint null,
+   strategy_type int null,
+   grant_type    int null,
+   grant_date    timestamp null,
+   grant_state   int null,
+   award_id      bigint null,
+   award_type    int null,
+   award_name    mediumtext null,
+   award_content mediumtext null,
+   uuid         mediumtext null,
+   create_time   timestamp null,
+   update_time   timestamp null
 ) comment '用户策略计算结果表';
 
 create table if not exists user_strategy_export_002
 (
     id           bigint(11) auto_increment comment '自增ID' primary key,
-    uId          mediumtext null,
-    activityId   bigint null,
-    orderId      bigint null,
-    strategyId   bigint null,
-    strategyType int null,
-    grantType    int null,
-    grantDate    timestamp null,
-    grantState   int null,
-    awardId      bigint null,
-    awardType    int null,
-    awardName    mediumtext null,
-    awardContent mediumtext null,
+    u_id          mediumtext null,
+    activity_id   bigint null,
+    order_id      bigint null,
+    strategy_id   bigint null,
+    strategy_type int null,
+    grant_type    int null,
+    grant_date    timestamp null,
+    grant_state   int null,
+    award_id      bigint null,
+    award_type    int null,
+    award_name    mediumtext null,
+    award_content mediumtext null,
     uuid         mediumtext null,
-    createTime   timestamp null,
-    updateTime   timestamp null
+    create_time   timestamp null,
+    update_time   timestamp null
 ) comment '用户策略计算结果表';
 
 create table if not exists user_strategy_export_003
 (
     id           bigint(11) auto_increment comment '自增ID' primary key,
-    uId          mediumtext null,
-    activityId   bigint null,
-    orderId      bigint null,
-    strategyId   bigint null,
-    strategyType int null,
-    grantType    int null,
-    grantDate    timestamp null,
-    grantState   int null,
-    awardId      bigint null,
-    awardType    int null,
-    awardName    mediumtext null,
-    awardContent mediumtext null,
+    u_id          mediumtext null,
+    activity_id   bigint null,
+    order_id      bigint null,
+    strategy_id   bigint null,
+    strategy_type int null,
+    grant_type    int null,
+    grant_date    timestamp null,
+    grant_state   int null,
+    award_id      bigint null,
+    award_type    int null,
+    award_name    mediumtext null,
+    award_content mediumtext null,
     uuid         mediumtext null,
-    createTime   timestamp null,
-    updateTime   timestamp null
+    create_time   timestamp null,
+    update_time   timestamp null
 ) comment '用户策略计算结果表';
 
 create table if not exists user_strategy_export_004
 (
     id           bigint(11) auto_increment comment '自增ID' primary key,
-    uId          mediumtext null,
-    activityId   bigint null,
-    orderId      bigint null,
-    strategyId   bigint null,
-    strategyType int null,
-    grantType    int null,
-    grantDate    timestamp null,
-    grantState   int null,
-    awardId      bigint null,
-    awardType    int null,
-    awardName    mediumtext null,
-    awardContent mediumtext null,
+    u_id          mediumtext null,
+    activity_id   bigint null,
+    order_id      bigint null,
+    strategy_id   bigint null,
+    strategy_type int null,
+    grant_type    int null,
+    grant_date    timestamp null,
+    grant_state   int null,
+    award_id      bigint null,
+    award_type    int null,
+    award_name    mediumtext null,
+    award_content mediumtext null,
     uuid         mediumtext null,
-    createTime   timestamp null,
-    updateTime   timestamp null
+    create_time   timestamp null,
+    update_time   timestamp null
 ) comment '用户策略计算结果表';
 
 
