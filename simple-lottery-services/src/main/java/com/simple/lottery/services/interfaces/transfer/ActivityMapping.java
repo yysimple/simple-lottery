4d59b7c7547c6c488b2ae66858299044553b7ada
wuchengxing <1449697757@qq.com>
2022-08-07 15:34:18 +0800

    feat：Mapping转换接口

---
+++ simple-lottery-services/src/main/java/com/simple/lottery/services/interfaces/transfer/ActivityMapping.java
@@ -0,0 +1,29 @@
+package com.simple.lottery.services.interfaces.transfer;
+
+import com.simple.lottery.domain.activity.model.vo.ActivityVO;
+import com.simple.lottery.rpc.dto.ActivityDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+/**
+ * 项目: simple-lottery
+ * <p>
+ * 功能描述: 活动对象转换配置
+ *
+ * @author: WuChengXing
+ * @create: 2022-08-07 15:20
+ **/
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public interface ActivityMapping extends IMapping<ActivityVO, ActivityDto> {
+
+    /**
+     * vo转换成dto
+     *
+     * @param activityVOS
+     * @return
+     */
+    @Override
+    List<ActivityDto> sourceToTarget(List<ActivityVO> activityVOS);
+}
---
+++ simple-lottery-services/src/main/java/com/simple/lottery/services/interfaces/transfer/AwardMapping.java
@@ -0,0 +1,14 @@
+package com.simple.lottery.services.interfaces.transfer;
+
+import com.simple.lottery.domain.strategy.model.vo.DrawAwardVO;
+
+/**
+ * 项目: simple-lottery
+ * <p>
+ * 功能描述:
+ *
+ * @author: WuChengXing
+ * @create: 2022-08-07 15:31
+ **/
+public interface AwardMapping extends IMapping<DrawAwardVO, AwardDto>{
+}
