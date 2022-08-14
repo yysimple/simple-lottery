80f261d28d49d099a23afa2b40c5da8bc2baf8cf
wuchengxing <1449697757@qq.com>
2022-08-14 16:09:22 +0800

    feat：kafka相关初始化

---
+++ simple-lottery-app/src/main/java/com/simple/lottery/app/mq/config/KafkaTopicProperties.java
@@ -0,0 +1,41 @@
+package com.simple.lottery.app.mq.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+/**
+ * 项目: simple-lottery
+ * <p>
+ * 功能描述: kafka的topic配置类
+ *
+ * @author: WuChengXing
+ * @create: 2022-08-14 16:00
+ **/
+@Configuration
+@ConfigurationProperties(prefix = "kafka")
+@Setter
+@Getter
+@ToString
+public class KafkaTopicProperties {
+
+    private List<Topic> topics;
+
+    @Setter
+    @Getter
+    @ToString
+    public static class Topic {
+        String name;
+        Integer numPartitions = 3;
+        Short replicationFactor = 1;
+        NewTopic toNewTopic() {
+            return new NewTopic(this.name, this.numPartitions, this.replicationFactor);
+        }
+    }
+
+}
---
+++ simple-lottery-app/src/main/java/com/simple/lottery/app/mq/config/MqInit.java
@@ -0,0 +1,45 @@
+package com.simple.lottery.app.mq.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+/**
+ * 项目: simple-lottery
+ * <p>
+ * 功能描述: mq的初始化相关
+ *
+ * @author: WuChengXing
+ * @create: 2022-08-14 15:51
+ **/
+@Configuration
+@EnableConfigurationProperties(KafkaTopicProperties.class)
+public class MqInit {
+    private final KafkaTopicProperties topicConfig;
+    private final GenericWebApplicationContext context;
+
+    public MqInit(KafkaTopicProperties topicConfig, GenericWebApplicationContext genericContext) {
+        this.topicConfig = topicConfig;
+        this.context = genericContext;
+    }
+
+    /**
+     * 项目启动时进行初始化
+     **/
+    @PostConstruct
+    public void init() {
+        initializeBeans(topicConfig.getTopics());
+    }
+
+    private void initializeBeans(List<KafkaTopicProperties.Topic> topics) {
+        topics.forEach(
+                t -> context.registerBean(t.name, NewTopic.class, t::toNewTopic)
+        );
+    }
+
+
+}
