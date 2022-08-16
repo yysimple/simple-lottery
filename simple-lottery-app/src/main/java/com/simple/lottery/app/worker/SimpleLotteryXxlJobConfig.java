2021034d3f05b9d6978279ffb54dcbcf0433c053
wuchengxing <1449697757@qq.com>
2022-08-16 15:54:39 +0800

    feat：xxl测试

---
+++ simple-lottery-app/src/main/java/com/simple/lottery/app/worker/SimpleLotteryXxlJobConfig.java
@@ -0,0 +1,58 @@
+package com.simple.lottery.app.worker;
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 项目: simple-lottery
+ * <p>
+ * 功能描述: xxl配置
+ *
+ * @author: WuChengXing
+ * @create: 2022-08-16 15:29
+ **/
+@Configuration
+public class SimpleLotteryXxlJobConfig {
+    private final Logger logger = LoggerFactory.getLogger(SimpleLotteryXxlJobConfig.class);
+
+    @Value("${xxl.job.admin.addresses}")
+    private String adminAddresses;
+
+    @Value("${xxl.job.executor.appname}")
+    private String appname;
+
+    @Value("${xxl.job.executor.address}")
+    private String address;
+
+    @Value("${xxl.job.executor.ip}")
+    private String ip;
+
+    @Value("${xxl.job.executor.port}")
+    private int port;
+
+    @Value("${xxl.job.executor.logpath}")
+    private String logPath;
+
+    @Value("${xxl.job.executor.logretentiondays}")
+    private int logRetentionDays;
+
+    @Bean
+    public XxlJobSpringExecutor xxlJobExecutor() {
+        logger.info(">>>>>>>>>>> xxl-job config init.");
+
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
+        xxlJobSpringExecutor.setAppname(appname);
+        xxlJobSpringExecutor.setAddress(address);
+        xxlJobSpringExecutor.setIp(ip);
+        xxlJobSpringExecutor.setPort(port);
+        xxlJobSpringExecutor.setLogPath(logPath);
+        xxlJobSpringExecutor.setLogRetentionDays(logRetentionDays);
+
+        return xxlJobSpringExecutor;
+    }
+}
--- simple-lottery-app/src/main/java/com/simple/lottery/app/worker/WorkerApp.java
+++ simple-lottery-app/src/main/java/com/simple/lottery/app/worker/WorkerApp.java
@@ -1,5 +1,12 @@
 package com.simple.lottery.app.worker;
 
+import com.xxl.job.core.handler.annotation.XxlJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
 /**
  * 项目: simple-lottery
  * <p>
@@ -8,5 +15,13 @@ package com.simple.lottery.app.worker;
  * @author: WuChengXing
  * @create: 2022-08-02 18:26
  **/
+@Component
 public class WorkerApp {
+
+    private final Logger logger = LoggerFactory.getLogger(WorkerApp.class);
+
+    @XxlJob("workerAppJobHandler")
+    public void workerAppJobHandler() throws Exception {
+        logger.warn("我是测试的定时任务，当前时间：{}", new Date());
+    }
 }
