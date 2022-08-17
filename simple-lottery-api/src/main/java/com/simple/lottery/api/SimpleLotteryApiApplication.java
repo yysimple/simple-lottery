702798fc7e97aa99a78ddeb1c9bbcf654e011e68
wuchengxing <1449697757@qq.com>
2022-08-17 16:45:33 +0800

    feat：初始化api应用

---
+++ simple-lottery-api/src/main/java/com/simple/lottery/api/SimpleLotteryApiApplication.java
@@ -0,0 +1,20 @@
+package com.simple.lottery.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目: simple-lottery-erp
+ * <p>
+ * 功能描述: api应用的启动类
+ *
+ * @author: WuChengXing
+ * @create: 2022-08-17 16:42
+ **/
+@SpringBootApplication
+public class SimpleLotteryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleLotteryApiApplication.class, args);
+    }
+}
